f5993823ce2c329de39517530b27c13ccd800f0d
Jan Chaloupka <jchaloup@redhat.com>
2016-03-26 03:02:24 +0100

    utils: introduce intervalsOverlap and intervalCovered

--- utils.py
+++ utils.py
@@ -14,5 +14,28 @@ def runCommand(cmd):
 	return stdout, stderr, rt
 
 def dateToTimestamp(date):
-	return time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple())
+	return int(time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple()))
 
+def intervalsOverlap(interval1, interval2):
+	s1, e1 = interval1
+	s2, e2 = interval2
+
+	# (s1, e1) < (s2, e2) or (s2, e2) < (s1, e1)
+	if e1 < s2 or e2 < s1:
+		return False
+
+	# in other cases intervals overlap
+	return True
+
+def intervalCovered(interval1, interval2):
+	"""Is interval1 covered by interval2?
+	I.e. is interval1 subset of interval2?
+
+	"""
+	s1, e1 = interval1
+	s2, e2 = interval2
+
+	if s2 <= s1 and e1 <= e2:
+		return True
+
+	return False
