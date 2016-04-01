438a3f376f255ad33c8d21246b56c4cdb7f09d69
Jan Chaloupka <jchaloup@redhat.com>
2016-04-01 15:11:31 +0200

    Extend utils with color codes

--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ import os
 import time
 import datetime
 
+RED = '\033[91m'
+GREEN = '\033[92m'
+BLUE = '\033[94m'
+CYAN = '\033[96m'
+WHITE = '\033[97m'
+YELLOW = '\033[93m'
+MAGENTA = '\033[95m'
+GREY = '\033[90m'
+BLACK = '\033[90m'
+DEFAULT = '\033[99m'
+ENDC = '\033[0m'
+
 def getScriptDir(file = __file__):
 	return os.path.dirname(os.path.realpath(file))
 
