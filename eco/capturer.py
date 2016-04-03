286832d29efd8322dda3d6cb2d51bcd9bb03b10e
Jan Chaloupka <jchaloup@redhat.com>
2016-04-03 23:32:59 +0200

    WIP: Introduce EcoCapturer provided latest distribution snapshots

---
+++ eco/capturer.py
@@ -0,0 +1,73 @@
+import logging
+from ..distributionsnapshot import DistributionSnapshot
+
+class EcoCapturer(object):
+	"""Capture state of golang projects in given distributions
+
+	"""
+
+	def __init__(self, koji_client, pkgdb_client):
+		"""Set the capturer with koji and pkgdb clients
+
+		:param koji_client: Koji client
+		:type  koji_client: KojiClient or FakeKojiClient
+		:param pkgdb_client: PkgDB client
+		:type  pkgdb_client: PkgDBClient or FakePkgDBClient
+		"""
+		self.koji_client = koji_client
+		self.pkgdb_client = pkgdb_client
+		self._snapshots = {}
+
+	def captureLatest(self, distributions, custom_packages):
+		"""Get the latest snapshot for requested distributions
+
+		:param distributions: list of distributions, each item as {"product": ..., "version": ...}
+		:type  distributions: [{}]
+		:param custom_packages: list of golang packages not prefixed with golang-*
+		:type  custom_packages: [string]
+		"""
+		self._snapshots = {}
+
+		# detect all golang packages with generic name
+		packages = self.pkgdb_client.getGolangPackages()
+
+		# scan packages
+		for distribution in distributions:
+			logging.info("Scanning %s:%s..." % (distribution["product"], distribution["version"]))
+			snapshot = DistributionSnapshot(distribution)
+			counter = 0
+			for package in packages.keys() + custom_packages:
+				# filter out all packages not in targeted distribution
+				# custom package inherits all available branches
+				if package in packages:
+					if distribution["version"] == "rawhide":
+						branch = "master"
+					else:
+						branch = distribution["version"]
+	
+					if branch not in packages[package]["branches"]:
+						continue
+
+				# get a list of rpms for given package
+				try:
+					data = self.koji_client.getLatestRPMS(distribution["version"], package)
+				except ValueError as e:
+					logging.error(e)
+					continue
+				except KeyError as e:
+					logging.error(e)
+					continue
+
+				snapshot.setRpms(package, data["rpms"])
+
+				counter = counter + 1
+				if counter > 3:
+					break
+
+			snapshot_key = "%s:%s" % (distribution["product"], distribution["version"])
+			self._snapshots[snapshot_key] = snapshot
+
+		return self
+
+	def snapshots(self):
+		return self._snapshots
