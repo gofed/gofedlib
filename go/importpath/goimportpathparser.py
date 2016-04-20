9f846988c8594e0105b4c5d585bfc2661f2ccb4e
Jan Chaloupka <jchaloup@redhat.com>
2016-04-20 12:03:33 +0200

    go/importpath: introduce GoImportPathParser

---
+++ go/importpath/goimportpathparser.py
@@ -0,0 +1,73 @@
+import json
+import re
+
+class GoImportPathParser(object):
+	"""Decide if a given import path is native (on of packages in golang stdlib).
+	If not, determine import path prefix based on a list of known prefixes.
+	"""
+
+	def __init__(self, known_ipprefixes, native_packages):
+		"""
+		:param known_prefixes: list of know ipprefixes, regexps expected
+		:type  known_prefixes: [regexp] with at least one group
+		:param native_packages: list of package prefixes from golang stdlib
+		:type  native_packages: [string]
+		"""
+		self.known_ipprefixes = known_ipprefixes
+		self.native_packages = native_packages
+
+		self._native = False
+		self._prefix = ""
+		self._package = ""
+
+	def parse(self, importpath):
+		"""Parse import path. Determine if the path is native or starts with known prefix.
+
+		:param importpath:	import path to parse
+		:type  importpath:	str
+		:return: bool
+		"""
+		# reset default values
+		self.native = False
+		self._prefix = ""
+		self._package = ""
+
+		url = re.sub(r'http://', '', importpath)
+		url = re.sub(r'https://', '', url)
+
+		# is import path native package?
+		if importpath.split('/')[0] in self.native_packages:
+			self.native = True
+			return self
+
+		for regex in self.known_ipprefixes:
+			match = re.search(regex, importpath)
+			if match:
+				self._prefix = match.group(1)
+				if match.group(3):
+					self._package = match.group(3)
+				return self
+
+		raise ValueError("Import path prefix not recognized")
+
+	def isNative(self):
+		return self.native
+
+	def prefix(self):
+		return self._prefix
+
+	def package(self):
+		return self._package
+
+if __name__ == "__main__":
+
+	with open("data/known_prefixes.json", "r") as f:
+		regexs = json.load(f)
+
+	with open("data/native_packages.json", "r") as f:
+		native = json.load(f)
+
+	p = GoImportPathParser(regexs, native)
+	p.parse("gopkg.in/v1/gcfg/scanner")
+	print p.prefix()
+	print p.package()
---
+++ go/importpath/test_goimportpathparser.py
@@ -0,0 +1,145 @@
+import unittest
+import json
+from goimportpathparser import GoImportPathParser
+
+class GoImportPathParserTest(unittest.TestCase):
+
+	def test(self):
+
+		paths = [{
+			"path": "github.com/coreos/etcd/pkg/netutil",
+			"prefix": "github.com/coreos/etcd",
+			"package": "pkg/netutil",
+			"native": False
+		},{
+			"path": "github.com/coreos/etcd/pkg/types",
+			"prefix": "github.com/coreos/etcd",
+			"package": "pkg/types",
+			"native": False
+		},{
+			"path": "github.com/coreos/go-systemd/daemon",
+			"prefix": "github.com/coreos/go-systemd",
+			"package": "daemon",
+			"native": False
+		},{
+			"path": "github.com/xiang90/probing",
+			"prefix": "github.com/xiang90/probing",
+			"package": "",
+			"native": False
+		},{
+			"path": "golang.org/x/crypto/bcrypt",
+			"prefix": "golang.org/x/crypto",
+			"package": "bcrypt",
+			"native": False
+		},{
+			"path": "golang.org/x/net/context",
+			"prefix": "golang.org/x/net",
+			"package": "context",
+			"native": False
+		},{
+			"path": "google.golang.org/grpc",
+			"prefix": "google.golang.org/grpc",
+			"package": "",
+			"native": False
+		},{
+			"path": "google.golang.org/grpc/credentials",
+			"prefix": "google.golang.org/grpc",
+			"package": "credentials",
+			"native": False
+		},{
+			"path": "code.google.com/p/goauth2/compute/serviceaccount",
+			"prefix": "code.google.com/p/goauth2",
+			"package": "compute/serviceaccount",
+			"native": False
+		},{
+			"path": "code.google.com/p/google-api-go-client/googleapi",
+			"prefix": "code.google.com/p/google-api-go-client",
+			"package": "googleapi",
+			"native": False
+		},{
+			"path": "bitbucket.org/kardianos/osext",
+			"prefix": "bitbucket.org/kardianos/osext",
+			"package": "",
+			"native": False
+		},{
+			"path": "gopkg.in/gcfg.v1/scanner",
+			"prefix": "gopkg.in/gcfg.v1",
+			"package": "scanner",
+			"native": False
+		},{
+			"path": "gopkg.in/v1/gcfg/scanner",
+			"prefix": "gopkg.in/v1/gcfg",
+			"package": "scanner",
+			"native": False
+		},{
+			"path": "gopkg.in/fsnotify.v0",
+			"prefix": "gopkg.in/fsnotify.v0",
+			"package": "",
+			"native": False
+		},{
+			"path": "bazil.org/fuse/aaa",
+			"prefix": "bazil.org/fuse",
+			"package": "aaa",
+			"native": False
+		},{
+			"path": "camlistore.org/aaa",
+			"prefix": "camlistore.org",
+			"package": "aaa",
+			"native": False
+		},{
+			"path": "collectd.org/aaa",
+			"prefix": "collectd.org",
+			"package": "aaa",
+			"native": False
+		},{
+			"path": "gopkg.in/fatih/pool.v2/aaa",
+			"prefix": "gopkg.in/fatih/pool.v2",
+			"package": "aaa",
+			"native": False
+		},{
+			"path": "gopkg.in/olivere/elastic.v2/aaa",
+			"prefix": "gopkg.in/olivere/elastic.v2",
+			"package": "aaa",
+			"native": False
+		},{
+			"path": "k8s.io/heapster/aaa",
+			"prefix": "k8s.io/heapster",
+			"package": "aaa",
+			"native": False
+		},{
+			"path": "k8s.io/kubernetes/aaa",
+			"prefix": "k8s.io/kubernetes",
+			"package": "aaa",
+			"native": False
+		},{
+			"path": "gopkg.in/natefinch/lumberjack.v1/aaa",
+			"prefix": "gopkg.in/natefinch/lumberjack.v1",
+			"package": "aaa",
+			"native": False
+		},{
+			"path": "launchpad.net/gocheck/check",
+			"prefix": "launchpad.net/gocheck",
+			"package": "check",
+			"native": False
+		},{
+			"path": "speter.net/go/exp/math/dec/inf/aaa",
+			"prefix": "speter.net/go/exp/math/dec/inf",
+			"package": "aaa",
+			"native": False
+		}]
+
+		with open("data/known_prefixes.json", "r") as f:
+			regexs = json.load(f)
+
+		with open("data/native_packages.json", "r") as f:
+			native = json.load(f)
+
+		p = GoImportPathParser(regexs, native)
+
+		for path in paths:
+			p.parse(path["path"])
+			self.assertEqual(path["native"], p.isNative())
+			if not path["native"]:
+				self.assertEqual(path["prefix"], p.prefix())
+				self.assertEqual(path["package"], p.package())
+
