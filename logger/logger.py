383ee12aecd8ca8ca811cf1715afb6fddea61558
Jan Chaloupka <jchaloup@redhat.com>
2016-04-30 21:27:20 +0200

    Move logging.yaml under logger directory. As logger comes from gofed_lib and gofed_infra/gofed uses the Logger class, all settings should come into logger/logger.yaml

--- logger/logger.py
+++ logger/logger.py
@@ -2,13 +2,19 @@ from lib.config.config import Config
 import yaml
 import logging
 import logging.config
+import os
+from lib.utils import getScriptDir
 
 class Logger(object):
 
 	@staticmethod
 	def set(verbose=False):
-		config_file = Config().loggingConfigFile()
-		log_config = yaml.load(open(Config().loggingConfigFile(), 'r'))
+		if "GOFED_DEVEL" in os.environ:
+			config_file = "%s/logging.yaml" % getScriptDir(__file__)
+		else:
+			config_file = Config().loggingConfigFile()
+
+		log_config = yaml.load(open(config_file, 'r'))
 		if verbose:
 			log_config["handlers"]["consoleHandler"]["level"] = "INFO"
 		logging.config.dictConfig(log_config)
